fa860cc4175a9bfc6bff4ac968cdab555eb24f02
srshkn <snuqlstyas@gmail.com>
2026-04-21 07:33:05 +0500

    update schema UserCreate

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,28 +1,11 @@
-from pydantic import EmailStr, Field, field_validator, model_validator
+from pydantic import Field
 
 from .schemas import APIModel
 
 
 class UserCreate(APIModel):
     name: str = Field(min_length=5, max_length=20)
-    email: EmailStr
     password: str = Field(min_length=5, max_length=75)
-    confirm_password: str = Field(min_length=5, max_length=75)
-
-    @field_validator("password")
-    @classmethod
-    def password_complexity(cls, value: str):
-        if " " in value:
-            raise ValueError("Пароль не должен содержать пробелы")
-        if value.isdigit():
-            raise ValueError("Пароль не может состоять только из цифр")
-        return value
-
-    @model_validator(mode="after")
-    def check_passwords_match(self) -> "UserCreate":
-        if self.password != self.confirm_password:
-            raise ValueError("Пароли не совпадают")
-        return self
 
 
 class UserOut(APIModel):
